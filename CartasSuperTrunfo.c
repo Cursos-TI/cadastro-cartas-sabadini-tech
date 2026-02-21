cc1ed0e3812cde49626a4cd96a6b9c2aef928183
sabadini-tech <sabadini.sa@gmail.com>
2026-02-21 16:51:14 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,8 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-    printf("Desafio Super Trunfo - Países");
-    
+    printf("Desafio Super Trunfo - Países\n");
+    printf("novo commit\n");
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
