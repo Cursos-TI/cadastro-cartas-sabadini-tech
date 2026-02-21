83e1729faa4a375018f67350feef2f3831619846
sabadini-tech <sabadini.sa@gmail.com>
2026-02-21 17:58:14 +0000

    criação carta 01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,11 +7,56 @@
 int main() {
     printf("Desafio Super Trunfo - Países\n");
     printf("novo commit\n");
+
+     
   // Área para definição das variáveis para armazenar as propriedades das cidades
+     int populaçao1;
+     int pontosturisticos1;
+     float area1;
+     float pib1;
+     char estado1;
+     char codigo1 [4];
+     char cidade1 [50];
 
   // Área para entrada de dados
+    printf("=== CADASTRO DA CARTA 1 ===\n");
+
+    printf("Estado: A");
+    scanf(" %c", &estado1);
+
+    printf("Código: \n");
+    scanf("%s", &codigo1);
+
+    printf("Nome da cidade: \n");
+    scanf("xs", &cidade1);
+
+    printf("População: \n");
+    scanf("%f", &populaçao1);
+
+    printf("Área: \n");
+    scanf("%f", &area1);
+    
+   printf("PIB: \n");
+   scanf("%f", &pib1);
+    
+   printf("Número de Pontos Turísticos: \n");
+   scanf("%d", &pontosturisticos1);
+    
+   printf("\n"); // Linha em branco para separar as cartas
+
 
   // Área para exibição dos dados da cidade
+
+  printf("=== DADOS DAS CARTAS CADASTRADAS ===\n\n");
+
+   printf("Carta 1:\n");
+   printf("Estado: %c\n", estado1);
+   printf("Código: %s\n", codigo1);
+   printf("Nome da Cidade: %s\n", cidade1);
+   printf("População: %d\n", populaçao1);
+   printf("Área: %.2f km²\n", area1);
+   printf("PIB: %.2f bilhões de reais\n", pib1);
+   printf("Número de Pontos Turísticos: %d\n", pontosturisticos1);
 
 return 0;
 } 
